cc82fbed286701417004dfcfe6db05fb474dec9b
swaswa999 <swayams@icloud.com>
2023-12-24 14:48:28 -0800

    got 6 pages working and removed routes as it was just added code

--- time_app/app.py
+++ time_app/app.py
@@ -1,11 +1,28 @@
 import os, random
-from flask import Flask
-from routes import home
+from flask import Flask,render_template, url_for
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return home()
+    return render_template('index.html')
+
+@app.route('/calender')
+def calender():
+    return render_template('calender.html')
+
+@app.route('/newtask')
+def new_task():
+    return render_template('new_task.html')
+
+@app.route('/setting')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
 
 if __name__ == '__main__':
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
--- time_app/routes.py
+++
@@ -1,4 +0,0 @@
-from flask import render_template
-
-def home():
-    return render_template('index.html')
