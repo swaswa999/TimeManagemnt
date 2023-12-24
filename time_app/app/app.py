a3050a117a233301bb93e14c988076e2cffa0fb7
swaswa999 <swayams@icloud.com>
2023-12-23 20:32:41 -0800

    troble shooting not being able to find the folder, also create routes.py to keep more organized

--- time_app/app/app.py
+++ time_app/app/app.py
@@ -1,13 +1,13 @@
 import os, random
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask
+from routes import home
 app = Flask(__name__)
 
 @app.route('/')
-def mainmenu():
-    return render_template('mainmenu.html')
+def index():
+    return home()
 
 if __name__ == '__main__':
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
     port = int(os.environ.get('FLASK_PORT', 81))
-    app.run(host=host, port=port, debug=True)  # Enable debug mode
+    app.run(host=host, port=port, debug=True)
