a1df4987689847086b745dec2c310d63a7b6d620
swaswa999 <swayams@icloud.com>
2023-12-28 21:53:43 -0800

    worked on adding task
    now able to add, record new subtasks and tasks with propper formating and time into a text file, will explore data baces tomorrow

--- time_app/app.py
+++ time_app/app.py
@@ -1,6 +1,6 @@
 import os, random
+from datetime import date
 from flask import Flask,render_template, redirect, request
-
 
 app = Flask(__name__)
 
@@ -17,8 +17,23 @@ def calender():
 @app.route('/newtask', methods=['GET', 'POST'])
 def new_task():
     if request.method == 'POST':
+        current_date = date.today()
 
-        print(request.form)
+        task = request.form.get('task_name')
+        date_added = current_date.strftime('%Y/%d/%m') #sets current date in yyyy/dd/mm
+        date_due = request.form.get('due_date')
+        subject = request.form.get('subject')
+        priority = request.form.get('priority') #scale of 1-3
+        location_added = 'manual' #when redoing scraper, indicates to not change this task
+        description = request.form.get('task_description')
+        subtasks = request.form.getlist('subtasks[]')  # Get a list of subtasks
+        subtask_times = request.form.getlist('subtask_times[]')  # Get a list of subtask times
+        status = 'not_complete' #shows that its a fresh task
+
+        with open('time_app/data/swayam.txt', 'a') as file:
+            file.write(f"{task} {date_added} {date_due} {subject} {priority} {location_added} {description} {subtasks} {subtask_times} {status} /n")
+
+
         return redirect('/')  
 
     return render_template('new_task.html')
